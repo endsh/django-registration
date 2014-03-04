60f3da4c10efd27c0d1c771d77455214a76cb4cf
endsh <438985635@qq.com>
2014-03-04 15:41:50 +0800

    fix django 1.6

--- registration/auth_urls.py
+++ registration/auth_urls.py
@@ -23,36 +23,33 @@ consult a specific backend's documentation for details.
 
 """
 
-from django.conf.urls.defaults import *
-
+from django.conf.urls import patterns, url
 from django.contrib.auth import views as auth_views
-
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-                       url(r'^login/$',
-                           auth_views.login,
-                           {'template_name': 'registration/login.html'},
-                           name='auth_login'),
-                       url(r'^logout/$',
-                           auth_views.logout,
-                           {'template_name': 'registration/logout.html'},
-                           name='auth_logout'),
-                       url(r'^password/change/$',
-                           auth_views.password_change,
-                           name='auth_password_change'),
-                       url(r'^password/change/done/$',
-                           auth_views.password_change_done,
-                           name='auth_password_change_done'),
-                       url(r'^password/reset/$',
-                           auth_views.password_reset,
-                           name='auth_password_reset'),
-                       url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
-                           auth_views.password_reset_confirm,
-                           name='auth_password_reset_confirm'),
-                       url(r'^password/reset/complete/$',
-                           auth_views.password_reset_complete,
-                           name='auth_password_reset_complete'),
-                       url(r'^password/reset/done/$',
-                           auth_views.password_reset_done,
-                           name='auth_password_reset_done'),
+	url(r'^activate/(?P<activation_key>\w+)/$', 'registration.views.activate'),
+	url(r'^login/$', auth_views.login,
+		{'template_name': 'registration/login.html'}),
+	url(r'^logout/$', auth_views.logout,
+		{'template_name': 'registration/logged_out.html'}),
+	url(r'^password/change/$', auth_views.password_change,
+		{'template_name': 'registration/password_change_form.html',
+		'post_change_redirect': 'done'}),
+	url(r'^password/change/done/$', auth_views.password_change_done,
+	 	{'template_name': 'registration/password_change_done.html'}),
+	url(r'^password/reset/$', auth_views.password_reset,
+		{'template_name': 'registration/password_reset.html',
+		'from_email': 'staff@openart.com'}),
+	url(r'^password/reset/done/$', auth_views.password_reset_done,
+		{'template_name': 'registration/password_reset_done.html'}),
+	url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+		auth_views.password_reset_confirm,
+		{'post_reset_redirect': '/accounts/password/reset/complete/'}),
+	url(r'^password/reset/complete/$',TemplateView.as_view(template_name='registration/password_reset_complete.html')),
+    url(r'^register/$', 'registration.views.register'),
+    url(r'^register/complete/$',TemplateView.as_view(template_name='registration/registration_complete.html')),
+
+
+    url(r'^profile/$', 'registration.views.profile'),
 )
